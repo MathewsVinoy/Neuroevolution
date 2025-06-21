55ebced3c46c58081ab47bffb3452c99cce317ce
MathewsVinoy <mathewsvinoy@gmail.com>
2025-06-21 22:40:20 +0530

    .....

---
+++ neat/connection.py
@@ -0,0 +1,15 @@
+class Connection:
+    """
+    Connection:
+     This class is used to creat an istance of the connection between 2 nodes.
+
+     self.enable => (dtype = boolen) diable when new node is added then added to the last of the Genome
+     innovation numbers =>(dtype = int) unique number reprsinting each connection
+    """
+    
+    def __init__(self,input_id,out_id,weight,enable,innovation_number):
+        self.inId = input_id
+        self.outId = out_id
+        self.weight = weight
+        self.enable = enable  
+        self.innoNo = innovation_number 
