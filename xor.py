a85e6c6fac2fd632b760be110a798d788e0df798
MathewsVinoy <mathewsvinoy@gmail.com>
2025-06-24 00:01:11 +0530

    created an xor game

--- xor.py
+++ xor.py
@@ -1,30 +1,20 @@
-import random
-from components import Node, Connection, Genome
+from neat.population import Population
+from neat.network import Network
+import pickle
 
-COUNT = 20
-input_list = [[0,0],[0,1],[1,0],[1,1]]
-output_list = [0,1,1,0]
+def train():
+    p = Population()
+    p.evolve(count=10, noGeneration=80, noInput=2, noOutput=1)  
 
 
+def load_model():
+    with open('models/model.pkl', 'rb') as f:
+        genome = pickle.load(f)
 
-innovation_numbers = 0
+    network = Network(genome=genome)
+    output = network.activate([1,0])
+    print(output)
 
-node_numbers = 0
-
-#In XOR there is 2 input and 1 output
-start_nodes=[]
-for n in range(2):
-    start_nodes.append(Node(idno=node_numbers,ntype='input',actfun='',bias=random.random()))
-    node_numbers+=1
-
-out = [Node(idno=node_numbers,ntype='output',actfun='',bias=0)]
-g=[]
-for i in range(COUNT):
-    genes_list =[]
-    conn = Connection(innovation_number=innovation_numbers,enable=True,
-                      input_id=start_nodes[random.randint(0,1)],out_id=output_list[0],
-                      weight=random.random())
-    innovation_numbers+=1
-    genes_list.append(conn)
-    g.append(Genome(connection=[genes_list],fitness=0,node=start_nodes+out))
-
+if __name__ == "__main__":
+    train()
+    load_model()
