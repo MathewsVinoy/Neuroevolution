e0f7c24ab6e4c201f61756da85a5e31aadebbed2
MathewsVinoy <mathewsvinoy@gmail.com>
2025-06-25 22:39:38 +0530

    ....

--- neat/fitness.py
+++ neat/fitness.py
@@ -1,4 +1,7 @@
 from neat.network import Network
+import pickle
+from pathlib import Path
+from random import randrange
 
 def evaluate_Fitness(phrnotype: Network):
     """
@@ -6,12 +9,21 @@ def evaluate_Fitness(phrnotype: Network):
     """
     input_list = [[0.0,0.0],[0.0,1.0],[1.0,0.0],[1.0,1.0]]
     output_list = [0.0,1.0,1.0,0.0]
-    total_error = 0.0
-    correct= 0
-    for i,out in zip(input_list,output_list):
-        pred = phrnotype.activate(i)
-        pred = pred[0]
-        total_error+=(out-pred)**2
-        if round(pred)==out:
-            correct+=1
-    return correct ** 2
+    fitness = 0 
+    for i in range(10):
+        value = randrange(len(input_list))
+        output = phrnotype.activate(input_list[value])
+        if round(output[0]) == output_list[value]:
+            fitness += 1
+        else:
+            fitness -=1
+    
+    return fitness
+
+
+
+def save_model(genome):
+    MODEL_PATH = Path("models")
+    MODEL_PATH.mkdir(parents=True, exist_ok=True)
+    with open('models/model.pkl', 'wb') as file:
+        pickle.dump(genome, file)
