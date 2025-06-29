545247fb972cf3789315fe092692b8f1b322a7db
MathewsVinoy <mathewsvinoy@gmail.com>
2025-06-29 16:55:57 +0530

    update

--- neat/population.py
+++ neat/population.py
@@ -1,5 +1,5 @@
 import random
-from neat.fitness import evaluate_Fitness
+from neat.fitness import save_model
 from neat.config import Config
 from neat.node import Node
 from neat.connection import Connection
@@ -8,11 +8,25 @@ from neat.specie import Specie
 from neat.network import Network
 from neat.activation import sigmoid_activation
 
-class Population:
+class Population(object):
+    evaluate_Fitness = None
+
+    
     def __init__(self):
         self.species = []
-        self.current_generation = 0
+        self.current_generation = -1
         self.genomes = []
+        self.best_fitness = []
+        self.avg_fitness =[]
+        self.create_population()
+        
+    
+    def create_population(self):
+        self.genome =[]
+        for i in range(Config.pop_size):
+            g = Genome.create_minimally_connected()
+            self.genome.append(g)
+
             
     def remove_members(self, species: Specie, percentage: float = 0.5):
         species.genomes.sort(key=lambda genome: genome.fitness)
@@ -27,14 +41,15 @@ class Population:
         
         input_nodes=[]
         for i in range(noInput):
-            n=Node(idno=Config.node_no,ntype='i')
-            n.actfun= sigmoid_activation
-            n.bias=random.randrange(-1,1)
-            input_nodes.append(n)
+           
+            input_nodes.append(Node(idno=Config.node_no,ntype='i'))
             Config.node_no +=1
         output_nodes=[]
         for i in range(noOutput):
-            output_nodes.append(Node(idno=Config.node_no,ntype='o'))
+            n=Node(idno=Config.node_no,ntype='o')
+            n.bias = random.random()
+            output_nodes.append(n)
+
             Config.node_no +=1
         
         self.genomes=[]
@@ -43,83 +58,90 @@ class Population:
             for i in output_nodes:
                 for j in  input_nodes:
                     inno=Config.innnovationTracker(j.id,i.id)
-                    conn_list.append(Connection(innovation_number=inno,enable=True,input_id=j.id,out_id=i.id,weight=random.randrange(-1,1)))
+                    conn_list.append(Connection(innovation_number=inno,enable=True,input_id=j.id,out_id=i.id,weight=random.random()))
             self.genomes.append(Genome(node=input_nodes+output_nodes,connection=conn_list))
-            
 
-    def evolve(self, count: int, noInput: int, noOutput: int, noGeneration: int):
-        self.initialize_population(count=count, noInput=noInput, noOutput=noOutput)
-
-        for generation in range(noGeneration):
-            print(f"Generation {generation + 1}")
-            
-
-            for genome in self.genomes:
-                phenotype = Network(genome=genome)
-                genome.fitness = evaluate_Fitness(phenotype)
-                print(f"Genome Fitness: {genome.fitness}")
-
-            self.species.clear()
-            for genome in self.genomes:
-                found_species = False
-                for specie in self.species:
-                    delta = specie.compatibilityDistance(genome)
-                    if delta < Config().compatibility_threshold:
-                        specie.genomes.append(genome)
-                        found_species = True
-                        break
-                if not found_species:
-                    new_species = Specie(genome)
-                    new_species.genomes.append(genome)
-                    self.species.append(new_species)
-
-            self.remove_stale_species()
+    def speciate(self):
+        for genome in self.genomes:
+            found = False
             for s in self.species:
-                self.remove_members(s)
+                if genome.distance(s.rep) < Config.compatibility_threshold:
+                    s.add(genome)
+                    found = True
+            if not found:
+                self.species.append(Specie(rep=genome))
 
-            for s in self.species:
-                for g in s.genomes:
-                    g.fitness = g.fitness / len(s.genomes)
+        self.set_compatibility_threshlod()
 
-            total_adjacent_fitness = sum(g.fitness for s in self.species for g in s.genomes)
-            print(f"Total Adjusted Fitness: {total_adjacent_fitness}")
-
-            new_generation_genomes = []
-            for s in self.species:
-                species_adj_fitness = sum(g.fitness for g in s.genomes)
-                num_offsprings = max(1, round(species_adj_fitness / total_adjacent_fitness * count))
-
-                for _ in range(num_offsprings):
-                    if len(s.genomes) >= 2:
-                        parent1, parent2 = random.sample(s.genomes, 2)
-                        if random.random() > 0.5:
-                            parent1.Crossover(parent2)
-                            child_node = parent1
-                        else:
-                            child_node = random.choice([parent1, parent2])
-                    else:
-                        child_node = random.choice(s.genomes)
-
-                    if child_node is not None:
-                        print("(")
-                        if random.random() < 0.5:
-                            print("1", end=" ")
-                            child_node.removeConnection()
-                        if random.random() < 0.5:
-                            print("2", end=" ")
-                            child_node.addConnection()
-                        if random.random() <0.2:
-                            print("3", end=" ")
-                            child_node.removeNode()
-                        if random.random() < 0.2:
-                            print("4", end=" ")
-                            child_node.addNode()
-                        new_generation_genomes.append(child_node)
-                        print(")")
-
-
-            self.genomes = new_generation_genomes
-            self.current_generation = generation + 1
-
-            
+    def set_compatibility_threshlod(self):
+        if len(self.species) > Config.species_size:
+            Config.compatibility_threshold += Config.compatibility_change
+        elif len(self.__species) < Config.species_size:
+            if Config.compatibility_threshold > Config.compatibility_change:
+                Config.compatibility_threshold -= Config.compatibility_change
+    def avg_fitness_fn(self):
+        sum = 0.0
+        for g in self.genome:
+            sum += g.fitness
+        
+        return sum/len(self.genome)
     
+    def compute_spawn_levels(self):
+        species_stats =[]
+        for s in self.species:
+            if s.age < Config.youth_threshold:
+                species_stats.append(s.average_fitness()*Config.youth_boost)
+            elif s.age > Config.old_threshold:
+                species_stats.append(s.average_fitness()*Config.old_penalty)
+            else:
+                species_stats.append(s.average_fitness())
+        total_average = 0.0 
+        for s in species_stats:
+            total_average +=5
+
+        for i, s in enumerate(self.species):
+            s.spawn_amount = int(round((species_stats[i]*Config.pop_size/total_average)))
+        
+    def log_species(self):
+        pass
+
+    def evolve(self, no):
+        for _ in range(no):
+            self.current_generation +=1
+            self.evaluate_Fitness()
+            self.speciate()
+            self.best_fitness.append(max(self.genomes))
+            self.avg_fitness.append(self.avg_fitness_fn())
+            best = self.best_fitness[-1]
+            for s in self.species:
+                s.hasBest = False
+                if best.id in s.genomes:
+                    s.hasBest = True
+            
+            save_model(best)
+
+            # remove the based performing genomes
+            for s in self.species[:]:
+                if s.no_improvement_age > Config.max_stagnation:
+                    if not s.hasBest:
+                        self.species.remove(s)
+                        for g in self.genome[:]:
+                            if g in s.genomes:
+                                self.remove(g)
+            
+            for s in self.species:
+                if s.no_inmprovement_age > 2 *Config.max_stagnation:
+                    self.species.remove(s)
+
+                    for g in self.genomes:
+                        if g in s.genomes:
+                            self.genomes.remove(g)
+
+            self.compute_spawn_levels()
+            for s in self.species:
+                if s.spawn_amount == 0:
+                    for g in self.genomes:
+                        if g in s.genomes:
+                            self.genomes.remove(s)
+
+            self.log_species()
