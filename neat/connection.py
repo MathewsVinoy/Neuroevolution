fd7c87bc90cb4ccfa93371ad3214e5823a4bc4a6
MathewsVinoy <mathewsvinoy@gmail.com>
2025-07-02 17:38:50 +0530

    ....

--- neat/connection.py
+++ neat/connection.py
@@ -1,23 +1,42 @@
+from random import random, gauss
+from neat.config import Config
+
 class Connection:
     """
     Connection:
-     This class is used to creat an istance of the connection between 2 nodes.
+    Represents a connection between two nodes in a NEAT genome.
 
-     self.enable => (dtype = boolen) diable when new node is added then added to the last of the Genome
-     innovation numbers =>(dtype = int) unique number reprsinting each connection
+    - `enable`: boolean, disabled when a new node splits the connection.
+    - `innoNo`: unique innovation number per (input_id, output_id) pair.
     """
     
-    def __init__(self,input_id,out_id,weight,enable,innovation_number):
+    # Shared innovation tracking across all Connection instances
+    innovation_tracker = {}
+    innovation_no = 0
+
+    def __init__(self, input_id, out_id, weight, enable):
         self.inId = input_id
         self.outId = out_id
         self.weight = weight
-        self.enable = enable  
-        self.innoNo = innovation_number 
+        self.enable = enable
+        self.innoNo = self.track_innovation(input_id, out_id)
 
-    @staticmethod
-    def innnovationTracker(innId, OutId):
-        key = (innId, OutId)
-        if key not in Config.innovation_tracker:
-            Config.innovation_tracker[key] = Config.innovation_no
-            Config.innovation_no += 1
-        return Config.innovation_tracker[key]
+    @classmethod
+    def track_innovation(cls, in_id, out_id):
+        key = (in_id, out_id)
+        if key not in cls.innovation_tracker:
+            cls.innovation_tracker[key] = cls.innovation_no
+            cls.innovation_no += 1
+        return cls.innovation_tracker[key]
+
+    def mutate(self):
+        if random() < Config.prob_mutate_weight:
+            self.mutate_weight()
+        if random() < Config.prob_togglelink:
+            self.enable = True  # Could toggle, but you force-enable here
+
+    def mutate_weight(self):
+        self.weight += gauss(0, 1) * Config.weight_mutation_power
+
+        # Clamp weight
+        self.weight = max(Config.min_weight, min(self.weight, Config.max_weight))
