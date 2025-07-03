6be9bae8d9a776ae1ef6208d9e683765dcb97899
MathewsVinoy <mathewsvinoy@gmail.com>
2025-07-03 09:13:58 +0530

    ............

--- neat/genome.py
+++ neat/genome.py
@@ -1,10 +1,11 @@
-from random import choice, randrange
+from random import choice, randrange, gauss, random
 from neat.node import Node
 from neat.connection import Connection
 from neat.config import Config
 from neat.graph import cycle_check
+import math
 
-class Genome:
+class Genome(object):
     """
     Genotype (Genome):
         Represents the genetic information of a neural network.
@@ -14,7 +15,80 @@ class Genome:
         self.nodes = node
         self.conn = connection
         self.fitness = 0
-        self.next_node_id = Config.node_no
+        self.next_node_id = Config.input_nodes + Config.hidden_nodes + Config.output_nodes
+
+    def mutate(self):
+        if random() < Config.prob_addnode:
+            self.addNode()
+        elif random() < Config.prob_addconn:
+            self.addConnection()
+        else:
+            for c in self.conn:
+                c.mutate()
+            for n in self.nodes:
+                n.mutate()
+
+    def distance(self, other):
+        if len(self.conn) > len(other):
+            parent1 = self.conn
+            parent2 = other
+        else:
+            parent1 = other
+            parent2 = self.conn
+
+        weight_diff =0
+        matching = 0
+        disjoint = 0
+        excess = 0
+
+        max_cg_parent2 = max(c.innoNo for c in parent2)
+        cg2 = {c.innoNo:c for c in parent2}
+        for c1 in parent1:
+            try:
+                c2 = cg2[c1.innoNo]
+            except KeyError:
+                if c1.innoNo > max_cg_parent2:
+                    excess += 1
+                else:
+                    disjoint +=1
+            else:
+                weight_diff += math.fabs(c1.weight- c2.weight)
+
+        disjoint += len(parent2) - matching
+
+        distance = Config.excess_coeficient * excess + Config.disjoint_coeficient * disjoint
+
+        if matching > 0:
+            distance += Config.weight_coeficient * (weight_diff/matching)
+
+        return distance
+    
+
+    @staticmethod
+    def create_minimally_connected():
+        input_nodes = []
+        id =0
+        for i in range(Config.input_nodes):
+            input_nodes.append(Node(id,'INPUT'))
+            id += 1
+
+        output_nodes = []
+        for i in range(Config.output_nodes):
+            output_nodes.append(Node(id,'OUTPUT'))
+            id += 1
+        
+        nodes=input_nodes+output_nodes
+        conn=[]
+        assert len(nodes) == id
+        for node in nodes:
+            if node.type != 'OUTPUT':
+                continue
+            input_node = choice(input_nodes)
+            wight = gauss(0,0.9)
+            conn.append(Connection(input_id=input_node.id,out_id=node.id,weight=wight,enable=True))
+        
+        return {"conn":conn,"node":nodes}
+
 
     def node_crossover(self, parent1: Node, parent2: Node) -> Node:
         """
@@ -37,15 +111,16 @@ class Genome:
         assert parent1.outId == parent2.outId
         weight = choice([parent1.weight, parent2.weight])
         enable = choice([parent1.enable, parent2.enable])
-        return Connection(
-            innovation_number=parent1.innoNo,
+        out = Connection(
             input_id=parent1.inId,
             out_id=parent1.outId,
             weight=weight,
             enable=enable
         )
+        out.innoNo = parent1.innoNo
+        return out
 
-    def Crossover(self, parent2):
+    def crossover(self, parent2):
         """
         Performs crossover between two genomes to produce a new genome.
         """
@@ -73,12 +148,7 @@ class Genome:
         self.nodes=new_nodes
         self.conn=new_conn
 
-            
-        
-        
-    
 
-    
     def addConnection(self):
         if len(self.nodes) < 2:
             return
@@ -88,11 +158,11 @@ class Genome:
         if n1.id == n2.id:
             return
         
-        if n1.type == 'o' and n2.type == 'o':
+        if n1.type == 'OUTPUT' and n2.type == 'OUTPUT':
             return
-        if n1.type == 'i' and n2.type == 'i':
+        if n1.type == 'INPUT' and n2.type == 'INPUT':
             return
-        if n1.type == 'o' or n2.type =='i':
+        if n1.type == 'OUTPUT' or n2.type =='INPUT':
             n1,n2 =n2, n1
         
         for c in self.conn:
@@ -102,12 +172,10 @@ class Genome:
         if cycle_check(self.conn, (n1.id, n2.id)):
             return
         
-        inno = Config.innnovationTracker(n1.id,n2.id)
         new_connection = Connection(
             input_id=n1.id,
             out_id=n2.id,
             enable=True,
-            innovation_number=inno,
             weight=randrange(-1, 1)
         )
         self.conn.append(new_connection)
@@ -125,21 +193,18 @@ class Genome:
             return
         old_conn = choice(self.conn)
         old_conn.enable = False
-        new_node = Node(idno=self.next_node_id, ntype='h')
-        self.next_node_id += 1
+        new_node = Node(idno=len(self.nodes), ntype='HIDDEN')
         self.nodes.append(new_node)
         new_conn1 = Connection(
             input_id=old_conn.inId,
             out_id=new_node.id,
             enable=True,
-            innovation_number=Config.innnovationTracker(old_conn.inId, new_node.id),
             weight=old_conn.weight
         )
         new_conn2 = Connection(
             input_id=new_node.id,
             out_id=old_conn.outId,
             enable=True,
-            innovation_number=Config.innnovationTracker(new_node.id, old_conn.outId),
             weight=1
         )
         self.conn.append(new_conn1)
