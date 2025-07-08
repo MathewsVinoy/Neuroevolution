928e7b66ef772ee99e95d74f7320ff26d67743bc
MathewsVinoy <mathewsvinoy@gmail.com>
2025-07-08 23:48:15 +0530

    update'

--- neat/node.py
+++ neat/node.py
@@ -3,27 +3,25 @@ from random import random, gauss
 from neat.config import Config
 
 class Node:
-    def __init__(self,idno:int, ntype: str):
+    def __init__(self, idno: int, ntype: str):
+        assert ntype in ('INPUT', 'OUTPUT', 'HIDDEN'), "Invalid node type."
         self.id = idno
         self.type = ntype
         self.actfun = sigmoid_activation
-        self.bias = 0
-        self.output =0.0
-        assert(self._type in ('INPUT', 'OUTPUT', 'HIDDEN'))
-    
+        self.bias = 0.0
+        self.output = 0.0
+        self.response = 4.924273  # default NEAT response gain
+
     def mutate(self):
         if random() < Config.prob_mutatebias:
             self.mutate_bias()
         if random() < Config.prob_mutatebias:
             self.mutate_response()
-    
 
     def mutate_bias(self):
-        self._bias += random.gauss(0,1)*Config.bias_mutation_power
-        if self._bias > Config.max_weight:
-            self._bias = Config.max_weight
-        elif self._bias < Config.min_weight:
-            self._bias = Config.min_weight
+        self.bias += gauss(0, 1) * Config.bias_mutation_power
+        self.bias = max(Config.min_weight, min(self.bias, Config.max_weight))
 
     def mutate_response(self):
-        self._response += random.gauss(0,1)*Config.bias_mutation_power
+        self.response += gauss(0, 1) * Config.bias_mutation_power
+        self.response = max(Config.min_weight, min(self.response, Config.max_weight))
